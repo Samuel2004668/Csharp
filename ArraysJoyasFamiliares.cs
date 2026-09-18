1e7fab82ca4da75e325010c01a2dde996066948b
Samuel2004668 <andressamuelgomez20@gmail.com>
2026-09-18 11:22:36 -0500

    Add NPCs and clues for different locations

---
+++ ArraysJoyasFamiliares.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Npcs
+{
+	public string escenario;
+	public string nombre;
+	public string dialogo;
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		Npcs[] Mansion= { Michael, Andrew, Policia};
+		String[] PistasMansion={"Falso","Falso","Frasco de Veneno"};
+		
+		Npcs[] Joyeria= { Lucy, Joseph, Asistente};
+		String[] PistasJoyeria={"Falso","Joyas Robadas","Herencia"};
+		
+		Npcs[] Comisaria= { Ana, Policia2,};
+		String[] PistasComisaria={"Grabaciones","Cuentas Bancarias de Michael y Andrew"};
+	}
+}
